3066b24a544783b53d1e677acb8ff26d0ab72fb8
QuickAndDirtyFOV <QuickAndDirtyFOV@3da71e8c-e694-e1a4-8d42-82aea26675ad>
2010-12-29 21:46:45 +0000

    Camera control

--- ir_tracking/src/testApp.cpp
+++ ir_tracking/src/testApp.cpp
@@ -1,102 +1,23 @@
 #include "testApp.h"
-#include "webcam.h"
+#include "cameracontrol_linux.h"
 
 #define SHOW_TRANSFORMED
 
-
-    CDevice *cdevs;
-    CControl *controls;
-    CHandle dev_handle;
-    CControlValue control_value;
-
-
 //--------------------------------------------------------------
 void testApp::setup(){
-
-
-
-    int result = 0;
-    unsigned int size = 164;
-    unsigned int control_size = 0;
-    unsigned int control_count = 0;
-    unsigned int count = 0;
-
-    cdevs = (CDevice *) malloc(sizeof(*cdevs) * size);
-
-	camWidth 		= 640;	// try to grab at this size.
+    int result = -1;
+    camWidth 		= 640;	// try to grab at this size.
 	camHeight 		= 480;
 	ofx_color_img.allocate(camWidth, camHeight);
-	c_init();
 
-	printf("Trying to list devices\n");
+	result = uvc_cams.setup();
 
-	if ((result = c_enum_devices(cdevs, &size, &count)) != C_SUCCESS) {
-        printf("Failed to enumerate devices on system %d %d!\n", result, size);
+	if (result < 0) {
+	    printf("Failed to setup cameras!\n");
+	    return;
     }
 
-    printf("Got %d devices\n", count);
-
-    /* Print CDevice info */
-    for (unsigned int i =0; i < count; i++) {
-    printf("Device %d info: sname = %s, name = %s, driver = %s,  location = %s\n", i,
-           cdevs[i].shortName, cdevs[i].name, cdevs[i].driver, cdevs[i].location);
-    }
-
-    dev_handle = c_open_device(cdevs[0].shortName);
-
-    if (dev_handle < 0) {
-        printf("Failed to open device!\n");
-        return;
-    }
-
-    /* Enumerate available controls */
-    c_enum_controls(dev_handle, NULL, &control_size, NULL);
-
-    controls = (CControl *) malloc(control_size);
-
-    if (C_SUCCESS != (result = c_enum_controls(dev_handle, controls, &control_size, &control_count))) {
-        printf("Failed to enumerate controls %d\n", result);
-        return;
-    }
-
-    /* Print CDevice info */
-    for (unsigned int i =0; i < control_count; i++) {
-    printf("Control info %d id = %d name = %s type = %d \n", i,
-           controls[i].id, controls[i].name, controls[i].type);
-
-           if (controls[i].type == 6) {
-               printf("Range: min = %d max = %d step = %d\n",
-                      controls[i].min.value, controls[i].max.value, controls[i].step.value);
-           }
-    }
-
-
-    /* Beef up exposure */
-    if (C_SUCCESS != c_get_control(dev_handle, CC_BRIGHTNESS, &control_value)) {
-            printf("Failed to get control!\n");
-    }
-
-    printf("Control info: type %d value %d\n", control_value.type, control_value.value);
-
-    /* Beef up exposure */
-    control_value.value = 255;
-    if (C_SUCCESS != (result = c_set_control(dev_handle, CC_BRIGHTNESS, &control_value))) {
-            printf("Failed to set control %d!\n", result);
-    }
-
-    if (C_SUCCESS != c_get_control(dev_handle, CC_BRIGHTNESS, &control_value)) {
-            printf("Failed to get control!\n");
-    }
-
-    printf("Control info: type %d value %d\n", control_value.type, control_value.value);
-
-    //c_close_device(dev_handle);
-
-    //c_cleanup();
-
-    printf("Enumerated devices!\n");
-
-
+    uvc_cams.set_brightness(-1, 100);
 
 	vidGrabber.setVerbose(true);
 	vidGrabber.listDevices();
@@ -109,23 +30,12 @@ void testApp::setup(){
 #endif
 
     printf("Setup complete!\n");
-
-    if (cdevs) {
-        free(cdevs);
-    }
-
 }
 
 //--------------------------------------------------------------
 void testApp::update(){
 
 	ofBackground(100,100,100);
-
-	if (C_SUCCESS != c_get_control(dev_handle, CC_BRIGHTNESS, &control_value)) {
-            printf("Failed to get control!\n");
-    }
-
-    printf("Control info: type %d value %d\n", control_value.type, control_value.value);
 
 	vidGrabber.grabFrame();
 #ifdef SHOW_TRANSFORMED
